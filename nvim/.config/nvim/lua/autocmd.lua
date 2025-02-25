dfddcc392b5371ef63e9d32687fc786184a11838
ali-molajani <amb137726@gmail.com>
2025-02-25 20:00:25 +0330

    write an autocmd to make nvim remember my latest cursor position like emacs

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -11,3 +11,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+-- Remember cursor position between sessions
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    local line = vim.fn.line
+    local last_pos = line '\'"'
+    if last_pos > 0 and last_pos <= line '$' then
+      -- Check if the file type is valid and not a commit message
+      local filetype = vim.bo.filetype
+      if filetype ~= 'gitcommit' and filetype ~= 'gitrebase' then
+        vim.cmd 'normal! g`"'
+      end
+    end
+  end,
+  pattern = '*',
+  desc = 'Remember cursor position when reopening a file',
+})
